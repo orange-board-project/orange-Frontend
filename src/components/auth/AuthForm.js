f0b2597dfa79d9505cb649dea22c761c1d3786c1
azure-553 <simmijin816@gmail.com>
2022-09-09 01:19:53 +0900

    ADD :: 로그인 폼 생성

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -1,14 +1,69 @@
 import React from "react";
 import styled from "styled-components";
+import Button from "../../common/Button";
 
 const AuthFormBlock = styled.div`
+    h3{
+        margin: 0;
+        color: #FEFEFE;
+        margin-bottom: 1rem;
+    }
 `
+const ButtonWhiteMarinTop = styled(Button)`
+    margin-top: 1rem;
+`
+const StyledInput = styled.div`
+    font-size: 1rem;
+    border:none;
+    border-bottom: 1px solid #FEFEFE;
+    padding-bottom: 0.5rem;
+    outline: none;
+    width: 100%;
+    &:focus{
+        color: $oc-teal-7;
+        border-bottom: 1px solid #FEFEFF;
+    }
+    &+&{
+        margin-top: 1rem;
+    }
+`
+const Footer = styled.div`
+    margin-top:2rem;
+    text-align : right;
+    a{
+        color : 1px solid #FEFEFF;
+        text-decoration: underline;
+        &:hover{
+            color: 1px solid #434343;
+        }
+    }
+`
+const textMap = {
+    login:'로그인',
+    register:'회원 가입'
+}
 
-const AuthForm = () => {
+const AuthForm = ({type}) => {
+    const text = textMap[type];
     return(
-        <div>
-            AuthForm
-        </div>
+        <AuthFormBlock>
+            <h3>{text}</h3>
+            <form>
+                <StyledInput
+                    autoComplete="username"
+                    name="username"
+                    placeholder="아이디"
+                />
+
+                <StyledInput
+                    autoComplete="current-password"
+                    name="password"
+                    placeholder="비밀번호"
+                    type="password"
+                />
+                {type === 're'}
+            </form>
+        </AuthFormBlock>
     );
 }
 
